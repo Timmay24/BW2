49708825a9171c3d733681c542cb072c378c1d15
Timmay24 <tim.hartig@haw-hamburg.de>
2014-12-05 09:27:02 +0100

    added paperclip, a picture and fields in views

--- compshop/app/controllers/articles_controller.rb
+++ compshop/app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
 
+  Paperclip.options[:command_path] = 'C:\Program Files\ImageMagick-6.9.0-Q16'
+
   # GET /articles
   # GET /articles.json
   def index
@@ -69,6 +71,6 @@ class ArticlesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def article_params
-      params.require(:article).permit(:name, :description, :category, :price_per_unit, :stock)
+      params.require(:article).permit(:name, :description, :category, :price_per_unit, :stock, :picture)
     end
 end
--- compshop/app/models/article.rb
+++ compshop/app/models/article.rb
@@ -1,3 +1,12 @@
+require 'paperclip/media_type_spoof_detector'
+module Paperclip
+  class MediaTypeSpoofDetector
+    def spoofed?
+        false
+    end
+  end
+end
+
 class Article < ActiveRecord::Base
   has_many :article_relations,  foreign_key: :super_id
   has_many :supers,             through: :article_relations
@@ -8,6 +17,18 @@ class Article < ActiveRecord::Base
   validates_presence_of :price_per_unit
   validates_presence_of :stock
   validates_uniqueness_of :name
+
+  has_attached_file :picture, :styles => { :thumb => "70x70", :small => "150x150>", :medium => "300x300", :large => "450x450" },
+                    :url  => "/assets/articles/:id/:style/:basename.:extension",
+                    :path => ":rails_root/public/assets/articles/:id/:style/:basename.:extension"
+
+  validates_attachment_presence :picture
+  validates_attachment_size :picture, :less_than => 5.megabytes
+  validates_attachment_content_type :picture, :content_type => ['image/jpeg', 'image/png']
+
+
+
+
 
 
   def self.search(article)
